1f19f1183557fb6a0744f896c60c88ef317395fb
Ish2504 <87877194+Ish2504@users.noreply.github.com>
2021-10-19 19:54:50 +0000

    1,2

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def caesar(start_text, shift_amount, cipher_direction):
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
@@ -24,7 +26,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n")) % 26
 
 #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 #Try running the program and entering a shift number of 45.
